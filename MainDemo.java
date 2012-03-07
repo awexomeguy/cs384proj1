94e5b5b6f9f02b82959c9fa9fb222d4d335d47f6
Chris <Owner@Owner-PC.(none)>
2012-03-06 21:58:22 -0600

    Visualizer complete. It makes some circles and moves them around in the path specified by the assignment. Speed can be controlled with sliders. No mutual exclusion implemented yet

--- MainDemo.java
+++ MainDemo.java
@@ -5,11 +5,10 @@ import javax.swing.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class MainDemo extends JApplet implements ActionListener
+public class MainDemo extends JApplet
 {
-	private Mover m;
-	private JButton startButton, stopButton;
-	private JSlider slider;
+	private Mover [] movers;
+	private JSlider [] sliders;
 	
 	Container c = getContentPane();
 	
@@ -17,45 +16,106 @@ public class MainDemo extends JApplet implements ActionListener
 	{
 		c.setLayout(new FlowLayout());
 		
-		startButton = new JButton("Start");
-		startButton.addActionListener(this);
+		// set up sliders to control speeds
+		sliders = new JSlider[4];
+		for(int i = 0; i < 4; ++i)
+		{
+			JPanel panel = new JPanel();
+			JLabel label = new JLabel();
+			
+			switch(i)
+			{
+				case 0:
+					label.setText("red");
+					break;
+				case 1:
+					label.setText("blue");
+					break;
+				case 2:
+					label.setText("green");
+					break;
+				case 3:
+					label.setText("orange");
+					break;
+			}
+			
+			sliders[i] = new JSlider(0, 10, 0);
+			sliders[i].setSnapToTicks(true);
+			panel.add(label);
+			panel.add(sliders[i]);
+			c.add(panel);
+		}
+
+		// set the movers' initial positions
+		movers = new Mover[4];
+		movers[0] = new Mover(this, sliders[0]);
+		movers[0].setPosition(MIN_X, 300);
+		movers[0].setDirection(Mover.UP);
 		
-		stopButton = new JButton("Stop");
-		stopButton.addActionListener(this);
+		movers[1] = new Mover(this, sliders[1]);
+		movers[1].setPosition(MIN_X, 350);
+		movers[1].setDirection(Mover.UP);
 		
-		slider = new JSlider(0, 10, 5);
-		slider.setSnapToTicks(true);
+		movers[2] = new Mover(this, sliders[2]);
+		movers[2].setPosition(MAX_X, 300);
+		movers[2].setDirection(Mover.DOWN);
 		
-		c.add(startButton);
-		c.add(stopButton);
-		c.add(slider);
+		movers[3] = new Mover(this, sliders[3]);
+		movers[3].setPosition(MAX_X, 350);
+		movers[3].setDirection(Mover.DOWN);
 		
-		m = new Mover(this, slider);
-		m.setPosition(275, 250);
-		
-		ExecutorService executor = Executors.newFixedThreadPool(1);
-		executor.execute(m);
+		// start all the movers
+		ExecutorService executor = Executors.newFixedThreadPool(4);
+		for(int i = 0; i < 4; ++i)
+			executor.execute(movers[i]);
 	}
 	
 	public void paint(Graphics g)
 	{
 		super.paint(g);
 		
-		g.drawOval(m.getX(), m.getY(), 50, 50);
-	}
-	
-	public void actionPerformed(ActionEvent e)
-	{
+		// draw the path for the movers
+		g.drawLine(MIN_X, MIN_Y, BRIDGE_LEFT, BRIDGE_Y);
+		g.drawLine(BRIDGE_LEFT, BRIDGE_Y, BRIDGE_RIGHT, BRIDGE_Y);
+		g.drawLine(BRIDGE_RIGHT, BRIDGE_Y, MAX_X, MIN_Y);
+		g.drawLine(MAX_X, MIN_Y, MAX_X, MAX_Y);
+		g.drawLine(MAX_X, MAX_Y, BRIDGE_RIGHT, BRIDGE_Y);
+		g.drawLine(BRIDGE_RIGHT, BRIDGE_Y, BRIDGE_LEFT, BRIDGE_Y);
+		g.drawLine(BRIDGE_LEFT, BRIDGE_Y, MIN_X, MAX_Y);
+		g.drawLine(MIN_X, MAX_Y, MIN_X, MIN_Y);
 		
+		//g.setColor(Color.RED);
+		// show the position of the movers
+		for(int i = 0; i < 4; ++i)
+		{
+			switch(i)
+			{
+				case 0:
+					g.setColor(Color.RED);
+					break;
+				case 1:
+					g.setColor(Color.BLUE);
+					break;
+				case 2:
+					g.setColor(Color.GREEN);
+					break;
+				case 3:
+					g.setColor(Color.ORANGE);
+					break;
+			}
+
+			g.fillOval(movers[i].getX() - 15, movers[i].getY() - 15, 30, 30);
+			g.drawString(movers[i].toString(), 10, 10 + i * 15);
+		}
 	}
 	
 	// the following parameters are used to define the path the Movers take
 	// the bridge is the critical section in this scenario
-	public int BRIDGE_LEFT_X;
-	public int BRIDGE_RIGHT_X;
-	public int BRIDGE_Y;
-	public int MAX_Y;
-	public int MIN_Y;
-	public int MAX_X;
-	public int MIN_X;
+	public static final int BRIDGE_LEFT = 300;
+	public static final int BRIDGE_RIGHT = 600;
+	public static final int BRIDGE_Y = 300;
+	public static final int MAX_Y = 500;
+	public static final int MIN_Y = 100;
+	public static final int MAX_X = 800;
+	public static final int MIN_X = 100;
 }
--- Mover.java
+++ Mover.java
@@ -7,6 +7,8 @@ public class Mover implements Runnable
 	protected JApplet a;
 	protected JSlider s;
 	protected int x, y; // coordinates of the Mover
+	protected int state; // state of the Mover (in CS, waiting for CS, or neither)
+	protected int direction; // current direction of Mover
 	
 	public Mover(JApplet app, JSlider slide)
 	{
@@ -16,14 +18,108 @@ public class Mover implements Runnable
 	
 	public void run()
 	{
-		for(int i = 0; i < 60; ++i)
+		while(true)
 		{
 			try
 			{
 				Thread.sleep(100);
 			} catch(InterruptedException ex){};
 			
-			setPosition(x + s.getValue(), y);
+			
+			if(direction == RIGHT) // we are currently moving right
+			{
+				if(x < MainDemo.BRIDGE_LEFT) // not yet to the bridge
+				{
+					setPosition(x + s.getValue(), y + s.getValue());
+					if(x >= MainDemo.BRIDGE_LEFT) // if we reach the bridge
+					{
+						setPosition(MainDemo.BRIDGE_LEFT, MainDemo.BRIDGE_Y);
+						state = WAITING;
+						state = IN_CS; // only to see if visualizer works, will remove
+					}
+				}
+				else if(x >= MainDemo.BRIDGE_LEFT && x < MainDemo.BRIDGE_RIGHT) // we are on the bridge
+				{
+					if(state == WAITING);
+						// mutual exclusion stuff here
+					else if(state == IN_CS)
+					{
+						setPosition(x + s.getValue(), MainDemo.BRIDGE_Y);
+						if(x >= MainDemo.BRIDGE_RIGHT) // if we reach the end of the bridge
+						{
+							setPosition(MainDemo.BRIDGE_RIGHT, MainDemo.BRIDGE_Y);
+							state = NEITHER;
+						}
+					}
+				}
+				else if(x >= MainDemo.BRIDGE_RIGHT) // we are past the bridge
+				{
+					setPosition(x + s.getValue(), y - s.getValue());
+					if(x >= MainDemo.MAX_X || y <= MainDemo.MIN_Y) // we reach the right edge, start going down
+					{
+						x = MainDemo.MAX_X;
+						y = MainDemo.MIN_Y;
+						direction = DOWN;
+					}
+				}
+			}
+			else if(direction == LEFT) // we are currently moving left
+			{
+				if(x > MainDemo.BRIDGE_RIGHT) // not yet to the bridge
+				{
+					setPosition(x - s.getValue(), y - s.getValue());
+					if(x <= MainDemo.BRIDGE_RIGHT) // if we reach the bridge
+					{
+						setPosition(MainDemo.BRIDGE_RIGHT, MainDemo.BRIDGE_Y);
+						state = WAITING;
+						state = IN_CS; // only to see if visualizer works, will remove
+					}
+				}
+				else if(x > MainDemo.BRIDGE_LEFT && x <= MainDemo.BRIDGE_RIGHT) // we are on the bridge
+				{
+					if(state == WAITING);
+						// mutual exclusion stuff here
+					else if(state == IN_CS)
+					{
+						setPosition(x - s.getValue(), MainDemo.BRIDGE_Y);
+						if(x < MainDemo.BRIDGE_LEFT) // if we reach the end of the bridge
+						{
+							setPosition(MainDemo.BRIDGE_LEFT, MainDemo.BRIDGE_Y);
+							state = NEITHER;
+						}
+					}
+				}
+				else if(x <= MainDemo.BRIDGE_LEFT) // we are past the bridge
+				{
+					setPosition(x - s.getValue(), y + s.getValue());
+					if(x <= MainDemo.MIN_X || y >= MainDemo.MAX_Y) // we reach the left edge, start going up
+					{
+						x = MainDemo.MIN_X;
+						y = MainDemo.MAX_Y;
+						direction = UP;
+					}
+				}
+			}
+			else if(direction == UP) // we are currently moving up
+			{
+				setPosition(x, y - s.getValue());
+				if(y < MainDemo.MIN_Y) // if we get to the top of the loop, turn right
+				{
+					y = MainDemo.MIN_Y;
+					direction = RIGHT;
+				}
+			}
+			else if(direction == DOWN)
+			{
+				setPosition(x, y + s.getValue());
+				if(y > MainDemo.MAX_Y) // if we get to the bottom of the loop, turn left
+				{
+					y = MainDemo.MAX_Y;
+					direction = LEFT;
+				}
+			}
+			
+			//repaint the applet to reflect the position change
 			a.repaint();
 		}
 	}
@@ -46,10 +142,47 @@ public class Mover implements Runnable
 	{ 
 		return y; 
 	}
+	
+	public int getState()
+	{
+		return state;
+	}
+	
+	public void setState(int n)
+	{
+		if(n >= 0 && n <= 2)
+			state = n;
+		else
+			return;
+	}
+	
+	public int getDirection()
+	{
+		return direction;
+	}
+	
+	public void setDirection(int n)
+	{
+		if(n >= 0 && n <= 3)
+			direction = n;
+		else
+			return;
+	}
 
 	// convert the position of the Mover into a String representation
 	public String toString() 
 	{ 
-		return "[" + x + ", " + y + "]"; 
+		return "[" + x + ", " + y + "]; " + direction + "; " + state; 
 	}
+	
+	// these represent the current direction the Mover is moving
+	public static final int LEFT = 0;
+	public static final int RIGHT = 1;
+	public static final int UP = 2;
+	public static final int DOWN = 3;
+	
+	// these represent the states of the Mover regarding the CS
+	public static final int WAITING = 0;
+	public static final int IN_CS = 1;
+	public static final int NEITHER = 2;
 }
